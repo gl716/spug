88c1b32cc600eed239f4a60b384db7d4a9bfd2c5
liang.guo <547044269@qq.com>
2023-03-13 17:09:33 +0800

    提交导入功能相关的改动

--- spug_api/apps/apis/importData.py
+++ spug_api/apps/apis/importData.py
@@ -3,6 +3,7 @@ import json
 from django.http import HttpResponse, Http404
 from apps.account.models import User
 from apps.app.models import App, Deploy, DeployExtend1
+from apps.host.models import Host
 from apps.config.models import Environment
 
 
@@ -11,9 +12,12 @@ def import_app(request):
     if request.method == 'POST':
         data = json.loads(request.body)
         env_map = {'1': 'dev', '2': 'test', '3': 'uat', '4': 'prod'}
+
         print(data['apps'])
         for t in data['apps']:
             environments = Environment.objects.filter(id=t[2]).all()
+            host = Host.objects.get(id=t[3].split(",")[0].rstrip())
+            default_dst_repo = f"/home/{host.username}/release"
             if not environments:
                 print(f'环境不存在：{t[2]}')
                 raise Http404
@@ -47,7 +51,7 @@ def import_app(request):
             deploy_extend1.deploy = deploy
             deploy_extend1.git_repo = t[4]
             deploy_extend1.dst_dir = t[5]
-            deploy_extend1.dst_repo = t[9] if t[9] else "release"
+            deploy_extend1.dst_repo = t[9] if t[9] else default_dst_repo
             deploy_extend1.versions = 5
             contain_rule = t[8] if t[8] else "*.jar"
             deploy_extend1.filter_rule = '{"type": "contain", "data": "' + contain_rule + '"}'
